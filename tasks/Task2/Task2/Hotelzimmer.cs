43447e08631dc96ee1bfe873190e3b7f4dfeaa9f
Jost92 <christoph.jost@a1telekom.at>
2016-05-09 19:33:25 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Hotelzimmer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Hotelzimmer
+    {
+
+        private decimal m_preis;
+
+        public Hotelzimmer(string name, decimal preis)
+
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Dem Zimmer muss ein Name vergeben werden.", nameof(name));
+
+            Name = name;
+            Preis = preis;
+
+        }
+
+     
+        /// <summary>
+        /// zeigt alle Namen und Preise der Hotels an
+        /// </summary>
+        public void ZeigePreisName()
+        {
+            Console.WriteLine($"name: {Name}");
+            Console.WriteLine($"preis: {Preis}");
+            
+        }
+
+        public string Name { get; }
+
+        public decimal Preis
+        {
+            get { return m_preis; }
+            set
+            {
+                if (value < 0) throw new Exception("Negativer Preis");
+                m_preis = value;
+            }
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var Zimmer1 = new Hotelzimmer("Edelstein", 100);
+                var Zimmer2 = new Hotelzimmer("Saphir", 250);
+                var Zimmer3 = new Hotelzimmer("Opal", 400);
+                Zimmer1.ZeigePreisName();
+                Zimmer2.ZeigePreisName();
+                Zimmer3.ZeigePreisName();
+                Zimmer3.Preis = 350;
+                Zimmer3.ZeigePreisName();
+            }
+
+            /*Wenn Fehler im Try passiert, springt er in den catch Zweig*/
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"ERROR: {ex}");
+            }
+
+        }
+    }
+}
